8b0eeb9d6a831fa76dc0c8951f6dc34019dde56f
Dekan <dekanbro@gmail.com>
2020-01-31 15:12:12 -0700

    add eth denver page

---
+++ src/views/ethDenver/EthDenver.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+const EthDenver = () => (
+<div className="View">
+    <h1>Hello EthDenver</h1>
+    <p>Things to do in Denver when your dead.</p>
+    <p>Get access to all our Moloch Contract Data with these sub graphs</p>
+    <ul>
+        <li>Factory: <a href="https://thegraph.com/explorer/subgraph/odyssy-automaton/daohaus-moloch-factory">https://thegraph.com/explorer/subgraph/odyssy-automaton/daohaus-moloch-factory/</a></li>
+        <li>Moloch: <a href="https://thegraph.com/explorer/subgraph/molochventures/moloch">https://thegraph.com/explorer/subgraph/molochventures/moloch</a></li>
+        <li>MetaCartel: <a href="https://thegraph.com/explorer/subgraph/jamesyoung/metacarteldao">https://thegraph.com/explorer/subgraph/jamesyoung/metacarteldao</a></li>
+    </ul>
+    <p>If you would like to submit a Dao directly to our factory use this </p>
+</div>);
+
+export default EthDenver;
